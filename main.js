f160fdbe8ef04f81b8df97b5655dae478c27e274
Ulugbek-Akmalovich-Shodimurodov <ulugbekshodimurodov16@gmail.com>
2022-09-30 16:17:47 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,58 @@
+// window.onload = function (){
+
+
+
+// let canvas = document.getElementById('canvas');
+// let context = canvas.getContext('2d');
+
+// let img = new Image();
+
+// img.src = './img/carousel-img.png';
+
+// img.onload = function(){
+
+//     // context.imageSmoothingEnabled = true;
+
+//     context.drawImage(img, -100, -100, 200, 200);
+    
+// }
+
+
+// context.moveTo(0, 00);
+// context.lineTo(600, 400);
+// context.stroke();
+
+
+// context.beginPath();
+// context.rect(50, 50, 200, 150);
+// context.lineWidth = 10;
+// context.fillStyle = "green";
+// context.strokeStyle = "red";
+// context.strokeStyle = "#0f0";
+// context.fill();
+// context.font = '20px Arial ';
+// context.fillStyle = "#00f";
+// context.fillText("salom Asad", 300, 300);
+
+// context.strokeStyle = "gray";
+// context.strokeText('Hello', 100, 150)
+// context.stroke();
+
+// context.beginPath();
+// context.arc(450, 150, 100, 0, 2* Math.PI)
+// context.stroke();
+
+
+// };
+
+// var canvas = document.getElementById("canvas"); 
+// var context = canvas.getContext("2d"); 
+// var x = 0; 
+// var y = 100; 
+ 
+// function draw() { 
+//   context.beginPath(); 
+//   context.rect(x, y, 100, 100); 
+//   context.fillStyle="red"; 
+//   context.fill(); 
+// }
